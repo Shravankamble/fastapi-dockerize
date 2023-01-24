dd7e58137f4823432d2b9ecd056b1f076b3ca4c7
shravan <kambleshravan014@gmail.com>
2023-01-25 01:29:29 +0530

    post route

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 users = []
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def index():
     return "hello world!"
 
-@app.get("/get/users")
+@app.get("/get/users", status_code=status.HTTP_200_OK)
 async def post():
     return users
 
-@app.post("/post/users")
+@app.post("/post/users", status_code=status.HTTP_201_CREATED)
 async def create_user(name: str):
     users.append(name)
     return f"new user : {name}"
